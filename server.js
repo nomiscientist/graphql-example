9002f1120b3cab0f71d8626772a77d6f70389355
nomi <getcompsci@gmail.com>
2022-07-31 13:16:20 +0500

    migrated to apollo graphql server

--- server.js
+++ server.js
@@ -2,23 +2,33 @@ const path = require('path');
 const express = require('express');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { loadFilesSync } = require('@graphql-tools/load-files');
-const { graphqlHTTP } = require('express-graphql');
 
-const app = express();
+const { ApolloServer } = require('apollo-server-express');
+
 
 const queryArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const resArray = loadFilesSync(path.join(__dirname, '**/*.resolver.js'));
 
-const schema = makeExecutableSchema({
-    typeDefs : queryArray,
-    resolvers: resArray
-});
+async function startApolloServer() {
 
-app.use('/graphql',graphqlHTTP({
-    schema: schema,
-    graphiql: true
-}));
+    const app = express();
 
-app.listen(3000, ()=>{
-    console.info('Listening on GraphQL server...');
-});
+    const schema = makeExecutableSchema({
+        typeDefs : queryArray,
+        resolvers: resArray
+    });
+
+    const server = new ApolloServer({
+        schema: schema,
+    });
+    
+    await server.start();
+    server.applyMiddleware({app, path: '/graphql'});
+
+    app.listen(3000, ()=>{
+        console.info('Listening on GraphQL server...');
+    });
+}
+
+startApolloServer();
+
